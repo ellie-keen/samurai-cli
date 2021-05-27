bc9ac3f184519e6a21654d096e5955f1a7913ca6
Ellie Keen <elliekeen95@gmail.com>
2021-05-27 22:31:32 +0100

    Add live-server package

--- bin/index.js
+++ bin/index.js
@@ -14,6 +14,110 @@ const samuraiQuotes = [
       'Engage in combat fully determined to die and you will be alive; wish to survive in the battle and you will surely meet death.',
     samurai: 'Uesugi Kenshin',
   },
+  {
+    quote: 'Victory is reserved for those who are willing to pay its price.',
+    samurai: 'Sun Tzu',
+  },
+  {
+    quote: 'Think lightly of yourself and deeply of the world.',
+    samurai: 'Miyamoto Musashi',
+  },
+  {
+    quote:
+      'See first with your mind, then with your eyes, and finally with your body.',
+    samurai: 'Yagyu Munenori',
+  },
+  {
+    quote:
+      "We don't rise to the level of our expectations, we fall to the level of our training.",
+    samurai: 'Archilochus',
+  },
+  {
+    quote:
+      'Without Knowledge, Skill cannot be focused. Without Skill, Strength cannot be brought to bear and without Strength, Knowledge may not be applied.',
+    samurai: 'Alexander the Great',
+  },
+  {
+    quote:
+      'The Samurai always has to rise and move on, because new challenges will come.',
+    samurai: 'Lyoto Machida',
+  },
+  {
+    quote:
+      "I'm not a fighter, but in my mind I'm fighting every day. 'What's new? What am I doing?' I'm fighting myself. My soul is samurai. My roots aren't samurai, but my soul is.",
+    samurai: 'Masaharu Morimoto',
+  },
+  {
+    quote: 'Civilize the mind but make savage the body.',
+    samurai: 'Mao Zedong',
+  },
+  {
+    quote: 'It does not matter how slowly you go as long as you do not stop.',
+    samurai: 'Confucius',
+  },
+  {
+    quote:
+      'Master the divine techniques of the Art of Peace and no enemy will dare to challenge you.',
+    samurai: 'Morihei Ueshiba',
+  },
+  {
+    quote: 'The way of the Samurai is found in death.',
+    samurai: 'Yamamoto Tsunetomo',
+  },
+  {
+    quote:
+      'Strategy without tactics is the slowest route to victory. Tactics without strategy is the noise before defeat.',
+    samurai: 'Sun Tzu',
+  },
+  {
+    quote:
+      "The uncomfort zone is very important for us... To feel uncomfortable is important to strengthen your spirit. Like the samurai says, if you feel comfortable you must search for the discomfort, don't look only for the easy way.",
+    samurai: 'Lyoto Machida',
+  },
+  {
+    quote:
+      'As a samurai, I must strengthen my character; as a human being I must perfect my spirit.',
+    samurai: 'Yamaoka Tesshu',
+  },
+  {
+    quote:
+      'The undisturbed mind is like the calm body water reflecting the brilliance of the moon. Empty the mind and you will realize the undisturbed mind.',
+    samurai: 'Yagyu Jubei Mitsuyoshi',
+  },
+  {
+    quote:
+      'Some Warriors look fierce, but are mild. Some seem timid, but are vicious. Look beyond appearances; position yourself for the advantage.',
+    samurai: 'Ming-Dao Deng',
+  },
+  {
+    quote:
+      'The perfect blossom is a rare thing. You could spend your life looking for one and it would not be a wasted life.',
+    samurai: 'Ken Watanabe',
+  },
+  {
+    quote:
+      'Mental bearing (calmness), not skill, is the sign of a matured samurai. A Samurai therefore should neither be pompous nor arrogant.',
+    samurai: 'Tsukahara Bokuden',
+  },
+  {
+    quote: 'Get beyond love and grief: exist for the good of Man.',
+    samurai: 'Miyamoto Musashi',
+  },
+  {
+    quote:
+      "A samurai should always be prepared for death - whether his own or someone else's.",
+    samurai: 'Stan Sakai',
+  },
+  {
+    quote:
+      'The dance of battle is always played to the same impatient rhythm. What begins in a surge of violent motion is always reduced to the perfectly still.',
+    samurai: 'Sun Tzu',
+  },
+  {
+    quote:
+      "My mentality is like a samurai they used to train every day, work on their technique to make themselves better, almost perfect, perfection is impossible but every day you get closer and that's what I want . Every day I want to get better than I was the day before. I want to use every second of my life, every time I have in my life to make me a better fighter. It's more than a job it's a way of living.",
+    samurai: 'Georges St-Pierre',
+  },
 ];
 
 const generateRandomNumber = (maximumValue) =>
